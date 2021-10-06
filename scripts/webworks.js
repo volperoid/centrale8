7c29f79a86c05e38b4b4050305bb3aceffb08d3c
volperoid <43482486+volperoid@users.noreply.github.com>
2021-10-06 06:30:14 +0200

    Update webworks.js

--- scripts/webworks.js
+++ scripts/webworks.js
@@ -2,8 +2,6 @@
 
     // * // Variables // * //
 
-    let accordionSlide = document.querySelectorAll(".slide");
-    let slidePanel = document.querySelectorAll(".description");
     let pageSection = document.getElementsByTagName("section");
     let mainHeader = document.getElementById("section-0");
     let footer = document.getElementsByTagName("footer");
@@ -16,19 +14,29 @@
     })();
 
     function accordion(target) {
+        let accordionSlide = document.querySelectorAll(".slide");
+        let slideOverlay = document.querySelector("#" + target.id + " " + ".ui-hint");
+        let slidePanel = document.querySelector("#" + target.id + " " + ".description");
         if (target.style.flex == "1 1 0%" || target.style.flex == "") {
-            document.querySelector("#" + target.id + " " + ".ui-hint").style.visibility = "hidden";
+            slideOverlay.style.visibility = "hidden";
+            slidePanel.style.transform = "translateX(0)";
+            slidePanel.style.opacity = 1;
             target.style.flex = "10 1 0%";
         } else if (target.style.flex == "10 1 0%") {
+            slideOverlay.style.visibility = "visible";
+            slidePanel.style.transform = "translateX(-100%)";
+            slidePanel.style.opacity = 0;
             target.style.flex = "1 1 0%";
         }
         for (let i = 0; i < accordionSlide.length; i++) {
+            let slidePanel = document.querySelector("#" + accordionSlide[i].id + " " + ".description");
             if (accordionSlide[i].id !== target.id) {
+                slidePanel.style.transform = "translateX(-100%)";
+                slidePanel.style.opacity = 0;
                 document.getElementById(accordionSlide[i].id).style.flex = "1 1 0%";
             }
         }
     }
-
     function showElement(element) {
         element.style.visibility = "visible";
     }
@@ -36,7 +44,7 @@
         element.style.visibility = "hidden";
     }
 
-    function close() {
+    function closeElement() {
         for (let i = 0; i < accordionSlide.length; i++) {
             if (accordionSlide[i].style.flex == "10 1 0%") {
                 accordionSlide[i].style.flex = "1 1 0%";
@@ -44,6 +52,7 @@
         }
 
     }
+
 
     // * // Event Listeners // * //
 
@@ -65,7 +74,7 @@
     for (let i = 0; i < pageSection.length; i++) {
         if (pageSection[i].id !== "section-2") {
             pageSection[i].addEventListener("click", () => {
-                close();
+                closeElement();
             });
         }
     }
